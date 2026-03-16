e45fb019e58e883b400a7600bd6d02b1d05cd3a9
Tatiana <t_sarnavskaya@mail.ru>
2026-03-16 12:48:34 +0300

    input/output of task info

---
+++ belhard_trainings/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, я таск-трекер!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название задачи:");
+        String taskName = scanner.nextLine();
+        System.out.print("Введите описание задачи:");
+        String description = scanner.nextLine();
+        System.out.print("Введите приоритет задачи:");
+        String priority = scanner.nextLine();
+        System.out.println("Информация о задаче:");
+        System.out.printf("Название задачи - %s;\n", taskName);
+        System.out.printf("Описание задачи - %s;\n", description);
+        System.out.printf("Приоритет задачи - %s;\n", priority);
+    }
+}
