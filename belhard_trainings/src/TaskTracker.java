a21fd765452aa34b1ba6ab88e12154dedf5a109b
Tatiana <t_sarnavskaya@mail.ru>
2026-03-16 13:04:04 +0300

    task1, task2

--- belhard_trainings/src/TaskTracker.java
+++ belhard_trainings/src/TaskTracker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TaskTracker {
     public static void main(String[] args) {
         System.out.println("Привет, я таск-трекер!");
         Scanner scanner = new Scanner(System.in);
